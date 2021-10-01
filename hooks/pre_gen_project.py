aa9318243f3a690bf8ed4eb272ea39c965718dcc
Mark Sevelj <mark.sevelj@dunwright.com.au>
2021-10-01 05:52:50 +0000

    chore(django): Add project up to rocketship #27
    Django project with Jinja templates up the rocketship stage in place.
    
    closes #27

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,7 +5,7 @@ import sys
 
 MODULE_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 
-MODULE_NAME = "{{ cookiecutter.project_slug}}"
+MODULE_NAME = "{{ cookiecutter.project_slug }}"
 
 if not re.match(MODULE_REGEX, MODULE_NAME):
     print(
