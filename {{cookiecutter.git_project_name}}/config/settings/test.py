5c86627980e98ead1eec49ef0ff728f439bf815c
mark sevelj <31756570+imAsparky@users.noreply.github.com>
2021-10-11 09:30:55 +0800

    test(django): Add DEBUG check & false to test config #114  (#118)
    * test(django): Add test config fast password hasher #104
    
    Speed up Django tests as per Django docs.
    
    WIP #104
    
    * test(django): Add pytest-django and pytest.ini #114
    
    Pytest has tools to simplify testing.
    pytest-django plugin improves pytest for django projects.
    
    WIP #114
    
    * test(django): Add DEBUG check & false to test config #114
    
    Testing in DEBUG mode slows tests due to overheads like
    django-debug-toolbar, which is generally not required for testing.
    
    Testing with DEBUG may be helpful on occasion for using django-debug-toolbar
    which can be used by changing DEBUG to true.
    
    WIP #114

--- {{cookiecutter.git_project_name}}/config/settings/test.py
+++ {{cookiecutter.git_project_name}}/config/settings/test.py
@@ -1,6 +1,11 @@
 """Django test settings for {{cookiecutter.git_project_name}} project."""
 
 from .base import *  # noqa
+from django.conf import settings
+
+DEBUG = False
+
+assert not settings.Debug, "DEBUG mode should be off"  # nosec
 
 # https://docs.djangoproject.com/en/3.2/topics/testing/overview/#speeding-up-tests-auth-hashers
 #
