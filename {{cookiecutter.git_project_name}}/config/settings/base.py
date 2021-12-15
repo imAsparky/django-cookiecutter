4b7379cd715c73f2947fb86c7ac7994b8455b773
mark sevelj <31756570+imAsparky@users.noreply.github.com>
2021-12-15 13:12:54 +0800

    fix(settings): correct DEBUG syntax #143 (#176)
    closes #143

--- {{cookiecutter.git_project_name}}/config/settings/base.py
+++ {{cookiecutter.git_project_name}}/config/settings/base.py
@@ -13,30 +13,9 @@ from pathlib import Path
 import os
 from django.utils.translation import ugettext_lazy as _
 from .username_blacklist import data as username_blacklist
-import environ
-
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    'DJANGO_SECRET_KEY', '!!!SET DJANGO_SECRET_KEY!!!')
-
-# SECURITY WARNING: don't run with debug turned on in production!
-# False if not in os.environ because of casting above
-DEBUG = env('DEBUG')
-
-INTERNAL_IPS = ["{{cookiecutter.INTERNAL_IPS}}"]
 
 # Application definition
 
@@ -108,16 +87,6 @@ AUTHENTICATION_BACKENDS = [
 {% endif %}
 WSGI_APPLICATION = "{{ cookiecutter.project_slug}}.wsgi.application"
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
@@ -187,6 +156,7 @@ TIME_ZONE = "{{cookiecutter.TIME_ZONE}}"
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
