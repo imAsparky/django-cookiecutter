56680904096560de20a4ceecedf978e4473448a2
mark sevelj <31756570+imAsparky@users.noreply.github.com>
2022-01-08 15:40:01 +0800

    feat(test): Add test DB options def=sqlite #227 (#228)
    * feat(test): Add test DB options def=sqlite #227
    
    Sometimes it is not convenient to run tests if a connection to a
    PostgreSQL, MySQL or other DB is unavailable.
    Local and or learning projects may not warrant a Production
    grade Database.
    Using an ENV to select between db.sqlite3 or a production database can
    make the testing process easier.
    
    closes #227
    
    * feat(test): Add test DB options def=sqlite #227
    
    Updated quickstart documentation.
    
    WIP #227

--- {{cookiecutter.git_project_name}}/config/settings/test.py
+++ {{cookiecutter.git_project_name}}/config/settings/test.py
@@ -15,8 +15,28 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 DEBUG = env("DJANGO_DEBUG", default=False)
 
-ALLOWED_HOSTS = ["{{cookiecutter.ALLOWED_HOSTS}}"]
+ALLOWED_HOSTS = [""]
 
+INTERNAL_IPS = [""]
+
+
+# Selects which database to use for testing, default=sqlite3 .
+TESTING_DATABASE = env("TESTING_DATABASE")
+
+if TESTING_DATABASE == "sqlite3":
+
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            # "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
+else:
+
+    DATABASES = {"default": env.db()}
+
+# Test with DEBUG off can speed up tests.
 assert not settings.DEBUG, "DEBUG mode should be off for testing."  # nosec
 
 DEFAULT_FILE_STORAGE = "inmemorystorage.InMemoryStorage"
