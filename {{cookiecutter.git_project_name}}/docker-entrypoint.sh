3cb0ea82dfd1326626b653fd3316de42e6afbc6d
Mark Sevelj <mark.sevelj@dunwright.com.au>
2023-10-02 10:36:10 +0800

    fix(shell): Docker entrypoint improvement #658
    Added checks for Database Environment variables and feedback to the user
    on what is happening in process of connecting to Postgres.
    
    closes #658

--- {{cookiecutter.git_project_name}}/docker-entrypoint.sh
+++ {{cookiecutter.git_project_name}}/docker-entrypoint.sh
@@ -1,14 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+# Get the correct Database URL.
 {% if cookiecutter.deploy_with_docker != "swarm" %}
-until psql $DATABASE_URL -c '\l'; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1
-done
+if [ -n "$LOCAL_DATABASE_URL" ]; then
+    CHECK_DATABASE_URL=$LOCAL_DATABASE_URL
+    DB_ENVIRONMENT="LOCAL DATABASE"
+elif [ -n "$TESTING_DATABASE_URL" ]; then
+    CHECK_DATABASE_URL=$TESTING_DATABASE_URL
+    DB_ENVIRONMENT="TESTING DATABASE"
+elif [ -n "$STAGING_DATABASE_URL" ]; then
+    CHECK_DATABASE_URL=$STAGING_DATABASE_URL
+    DB_ENVIRONMENT="STAGING DATABASE"
+elif [ -n "$PROD_DATABASE_URL" ]; then
+    CHECK_DATABASE_URL=$PROD_DATABASE_URL
+    DB_ENVIRONMENT="PRODUCTION DATABASE"
+elif [ -n "$DATABASE_URL" ]; then
+    CHECK_DATABASE_URL=$DATABASE_URL
+    DB_ENVIRONMENT="DB ENV NOT SUPPLIED. USING DEFAULT URL"
+
+fi
+
+if [ -n "$CHECK_DATABASE_URL" ]; then
+    >&2 echo "OH NICE.....THERE IS A DATABASE URL HERE MATE"
+    # Check to see if the Database is available.
+    until psql "$CHECK_DATABASE_URL" -c '\l'; do
+    >&2 echo "Checking Postgres availability."$'\n'"DB ENVIRONMENT: {$DB_ENVIRONMENT}"$'\n'"Postgres DB is unavailable - sleeping 10!"
+    sleep 10
+    done
+else
+
+    >&2 echo "DUOH.....THERE IS NO DATABASE URL HERE MATE!"$'\n'"EXITING!!!"
+    exit 1
+
+fi
 
 >&2 echo "Postgres is up - continuing"
 {% endif %}
 if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
+    >&2 echo "Postgres is up - Attempting to migrate."
     python manage.py migrate --noinput
 fi
 
